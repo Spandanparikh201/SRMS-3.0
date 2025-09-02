d75ab9a40e1d6d1e0858182133b9b53a729a06ae
SpandanParikh201 <spandyparikh@gmail.com>
2025-09-02 22:42:49 +0530

    Update database connection configuration

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,20 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "srms_db";
+$servername = "db.fr-pari1.bengt.wasmernet.com";
+$username = "7239bada7d21800036f6f3e48a3f";
+$password = "068b7239-bada-7e94-8000-f5018332d94e";
+$dbname = "srms_db1";
+$port = 10272;
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Create a new MySQLi connection
+try {
+    $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    // Check connection
+    if ($conn->connect_error) {
+        throw new mysqli_sql_exception($conn->connect_error, $conn->connect_errno);
+    }
+} catch (mysqli_sql_exception $e) {
+    // This will catch the connection error and provide a more informative message
+    die("Connection failed: " . $e->getMessage());
 }
 ?>
